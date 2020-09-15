87179c8a1f120c7487c3a5ad95e36afa9eab6c1d
koby5880 <koby5880@ISS2TSRKH2.scdsb.pri>
2020-09-15 12:35:19 -0400

    Initial Commit

--- src/PizzaCost1.java
+++ src/PizzaCost1.java
@@ -1,10 +1,32 @@
 
+import java.util.Scanner;
+
+
 public class PizzaCost1 {
 
     
     public static void main(String[] args) {
-        
-        
+         int toppings;
+         double inches, pizzacost, gascost, total;
+         
+         
+         Scanner s = new Scanner(System.in);
+         
+         //Input
+         System.out.print("Enter the size of the pizza (in Inches) >> ");
+         inches = s.nextInt();
+         
+         System.out.print("Enter the number of toppings >> ");
+         toppings = s.nextInt();
+         
+         //Output
+         pizzacost = 1.50;
+         gascost = 0.75;
+         
+         total = pizzacost + gascost + (toppings * 0.75)+(inches * 0.50);
+         System.out.print(String.format("The total cost is: %.2f", total));
+         
+         
     }
     
 }
